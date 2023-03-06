9d9291aa9d3b5fb237b5f8232f82be776618fcb5
Letalys <chris.goemaere@live.fr>
2023-03-06 23:45:10 +0100

    Add TryCatch in MainCode

--- RebootReminder.ps1
+++ RebootReminder.ps1
@@ -19,65 +19,72 @@
 $MainCode = {
     Param([Parameter(Mandatory=$true)]$XAMLDoc)
 
-    [xml]$XAML = [IO.File]::ReadAllText($XAMLDoc,[Text.Encoding]::GetEncoding(65001))
-    
-    #Creation of a Thread-Safe synchronization HashTable, allows to retrieve objects and properties by any RunSpace (Execution space)
-    $syncHash = [hashtable]::Synchronized(@{})
+    try{
+        [xml]$XAML = [IO.File]::ReadAllText($XAMLDoc,[Text.Encoding]::GetEncoding(65001))
+        
+        #Creation of a Thread-Safe synchronization HashTable, allows to retrieve objects and properties by any RunSpace (Execution space)
+        $syncHash = [hashtable]::Synchronized(@{})
 
-    #Load XAML and Add to Hashtable
-    $XAMLReader=(New-Object System.Xml.XmlNodeReader $XAML)
-    $syncHash.Window=[Windows.Markup.XamlReader]::Load($XAMLReader)
+        #Load XAML and Add to Hashtable
+        $XAMLReader=(New-Object System.Xml.XmlNodeReader $XAML)
+        $syncHash.Window=[Windows.Markup.XamlReader]::Load($XAMLReader)
 
-    #Calculate WPF Window position at bottom right
-    $screen = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea
-    $syncHash.Window.Left = $screen.Right - $syncHash.Window.Width
-    $syncHash.Window.Top = $screen.Bottom - $syncHash.Window.Height
+        #Calculate WPF Window position at bottom right
+        $screen = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea
+        $syncHash.Window.Left = $screen.Right - $syncHash.Window.Width
+        $syncHash.Window.Top = $screen.Bottom - $syncHash.Window.Height
 
-    #Set topmost
-    $syncHash.Window.TopMost = $true
+        #Set topmost
+        $syncHash.Window.TopMost = $true
 
-    #Get the last time reboot
-    $DiffTimeSpan = New-TimeSpan $(Get-Date ((Get-CimInstance -Class Win32_OperatingSystem).LastBootUpTime)) $(Get-Date)
+        #Get the last time reboot
+        $DiffTimeSpan = New-TimeSpan $(Get-Date ((Get-CimInstance -Class Win32_OperatingSystem).LastBootUpTime)) $(Get-Date)
 
-    #Get Rounded up day
-    $days = [math]::Round($DiffTimeSpan.TotalDays)
-    $DaysDigit = $days.ToString().ToCharArray()
+        #Get Rounded up day
+        $days = [math]::Round($DiffTimeSpan.TotalDays)
+        $DaysDigit = $days.ToString().ToCharArray()
 
-    $syncHash.Window.FindName("FormUI").Add_ContentRendered({
-        $syncHash.Window.FindName("LBL_ComputerName").Content = $env:COMPUTERNAME
-        Switch($true){
-             ($DaysDigit.length -eq 1){
-                 $syncHash.Window.FindName("LBL_DayDigit1").Content = $DaysDigit[0]
-                 $syncHash.Window.FindName("LBL_DayDigit2").Content = 0
-                 $syncHash.Window.FindName("LBL_DayDigit2").Content = 0
-             }
-             ($DaysDigit.length -eq 2){
-                 $syncHash.Window.FindName("LBL_DayDigit1").Content = $DaysDigit[0]
-                 $syncHash.Window.FindName("LBL_DayDigit2").Content = $DaysDigit[1]
-                 $syncHash.Window.FindName("LBL_DayDigit2").Content = 0
-             }
-             ($DaysDigit.length -eq 3){
-                 $syncHash.Window.FindName("LBL_DayDigit1").Content = $DaysDigit[0]
-                 $syncHash.Window.FindName("LBL_DayDigit2").Content = $DaysDigit[1]
-                 $syncHash.Window.FindName("LBL_DayDigit2").Content = $DaysDigit[2]
-             }
-        }
-    })
+        $syncHash.Window.FindName("FormUI").Add_ContentRendered({
+            $syncHash.Window.FindName("LBL_ComputerName").Content = $env:COMPUTERNAME
+            Switch($true){
+                ($DaysDigit.length -eq 1){
+                    $syncHash.Window.FindName("LBL_DayDigit1").Content = $DaysDigit[0]
+                    $syncHash.Window.FindName("LBL_DayDigit2").Content = 0
+                    $syncHash.Window.FindName("LBL_DayDigit3").Content = 0
+                }
+                ($DaysDigit.length -eq 2){
+                    $syncHash.Window.FindName("LBL_DayDigit1").Content = $DaysDigit[0]
+                    $syncHash.Window.FindName("LBL_DayDigit2").Content = $DaysDigit[1]
+                    $syncHash.Window.FindName("LBL_DayDigit3").Content = 0
+                }
+                ($DaysDigit.length -eq 3){
+                    $syncHash.Window.FindName("LBL_DayDigit1").Content = $DaysDigit[0]
+                    $syncHash.Window.FindName("LBL_DayDigit2").Content = $DaysDigit[1]
+                    $syncHash.Window.FindName("LBL_DayDigit3").Content = $DaysDigit[2]
+                }
+            }
+        })
 
-    $Global:Selection = $null
-    $syncHash.Window.FindName("B_RebootNow").Add_Click({
-        $Global:Selection = 1
-        $syncHash.Window.close()
-    })
+        $Global:Selection = $null
+        $syncHash.Window.FindName("B_RebootNow").Add_Click({
+            $Global:Selection = 1
+            $syncHash.Window.close()
+        })
 
-    $syncHash.Window.FindName("B_RebootLater").Add_Click({
-        $Global:Selection = 2
-        $syncHash.Window.close()
-    })
+        $syncHash.Window.FindName("B_RebootLater").Add_Click({
+            $Global:Selection = 2
+            $syncHash.Window.close()
+        })
 
-    #Show Window
-    $syncHash.Window.ShowDialog() | Out-Null
-    Write-Output $Days $Global:Selection
+        #Show Window
+        $syncHash.Window.ShowDialog() | Out-Null
+        Write-Output $Days
+        Write-Output $Global:Selection
+
+    }catch{
+        Write-Output $_
+        Write-Output -1
+    }
 }
 
 #Creating runspace
